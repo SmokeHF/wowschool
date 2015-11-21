c3cecbe61517aa49de09397dc87e67ba9ab45fa6
smokehf <tomaszmarczynski69@gmail.com>
2015-11-21 15:57:13 +0100

    change

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,10 +5,10 @@
  *
  */
 public class main {
-	private static void main(String[] args) 
-	example localexampleVarieble0 = new example() 
+	private static void main(String[] args) {
+	example localexampleVarieble0 = new example() {
 			example localexampleVarieble1 =new example()
 	{
-	
-	
-}
+				{
+
+				}
